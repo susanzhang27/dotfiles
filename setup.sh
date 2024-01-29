3fade15828268f7eb92599d09f63bd6868a9be83
Susan Zhang <32766206+susanzhang27@users.noreply.github.com>
2024-01-29 12:22:00 -0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Magical git log tree from https://stackoverflow.com/questions/1838873/visualizing-branch-topology-in-git/34467298#34467298
+git config --global alias.lg "lg1"
+git config --global alias.lg1 "lg1-specific --all"
+git config --global alias.lg2 "lg2-specific --all"
+git config --global alias.lg3 "lg3-specific --all"
+
+git config --global alias.lg1-specific "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'"
+git config --global alias.lg2-specific "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'"
+git config --global alias.lg3-specific "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset) %C(bold cyan)(committed: %cD)%C(reset) %C(auto)%d%C(reset)%n''          %C(white)%s%C(reset)%n''          %C(dim white)- %an <%ae> %C(reset) %C(dim white)(committer: %cn <%ce>)%C(reset)'"
